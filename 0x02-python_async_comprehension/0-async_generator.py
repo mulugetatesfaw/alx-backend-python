945730c6f3c85fc27e979f3766f22ed24513c56d
mulugetatesfaw <mulugetatesfaw21@gmail.com>
2023-08-08 02:42:54 -0700

    Write a coroutine called async_generator that takes no arguments.

---
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+""" Async Generator ports"""
+import asyncio
+import random
+from typing import Generator
+
+
+async def async_generator() -> Generator[float, None, None]:
+    """
+        Generate numbers
+
+        Args: No
+
+        Return:
+            float time random
+    """
+    for _ in range(10):
+        await asyncio.sleep(1)
+        yield random.uniform(0, 10)
